06a4904aa9d0cac1aa8941601dbdaf5ab1cf10f1
Navod Kamilka <navod@tsdclanka.com>
2023-02-01 16:07:59 +0530

    update

--- Server/validations/post.validation.js
+++ Server/validations/post.validation.js
@@ -1,10 +1,5 @@
 import joi from 'joi';
 
 export const addProductValidation = joi.object({
-    sku:joi.string().required(),
-    name:joi.string().required(),
-    description:joi.allow(),
-    categorieId:joi.number().integer().required(),
-    subCategoryId:joi.number().integer().required(),
-    price:joi.required()
+    
 });
